d49911a3f63acbff61f46f4aae4f277235f499d3
Guidek23 <jirajade.kor@student.mahidol.edu>
2025-10-02 04:44:51 +0000

    finish

--- main.cpp
+++ main.cpp
@@ -6,33 +6,70 @@ using namespace std;
 
 int main(int argc,char *argv[]){
   int *pa, *pb, i, temp;
-int a[SIZE] ={1,2,3,4,5,6,7,8,9,10}; 
-pa = &a[0]; //pa =a;
+  int n = argc - 1;
+
+
+/*
+change the size to dynamic size
+
+Allocate (new) value according to size 
+delete a[]
+use 
+int *p= new int[n]
+
+input all the values in those new spaces.
+*/
+
+
+pa = new int [n];
+pb = new int [n];
+
+for (i = 0; i < n; i++)
+{
+  *pa = atoi(argv[i+1]);
+  pa++;
+}
+
+/* old version
+
+cout << "How many numbers?" << endl; // get the number from the user
+cin >> n;
+for (i = 0; i < n; i++)
+{
+  cout << "Input no." << i+1 << ": ";
+  cin >> *pa; 
+  pa++;
+}
+*/
+
+pa-=n;
+
 cout<<"Original: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
+for (i=0; i<n-1; i++ ,pa++){
    cout<<setw(3)<<*pa ;//<<endl;
   //cout<<pa<<endl;
 }
-cout<<setw(3)<<*pa<<endl;
+cout<<setw(3)<<*pa<<endl; //
 
-pa = &a[0];  
-pb = &a[SIZE-1];
-for (i=0; i<SIZE/2; i++)
+pb = pa;
+pa-= (n-1);
+for (i=0; i<n/2; i++)
 {
    temp = *pa;  
    *pa = *pb;  
    *pb = temp;
    pa++;  pb--;
 }
-pa=&a[0];
-pb-=SIZE/2;
+
+pa -= (n/2);
+
 cout<<"Reversed: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
+for (i=0; i<n-1; i++ ,pa++){
    cout<<setw(3)<<*pa ;//<<endl;
   //cout<<pa<<endl;
 }
 cout<<setw(3)<<*pa<<endl;
 
-
+delete []pa; //delete the n array.
 return 0;
 }
